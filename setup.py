058f52a688db31778326f62a518d8e0de71c2a64
doug <doug.shawhan@gmail.com>
2017-09-03 00:14:02 -0400

    package

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup
 
 setup(name="Quartermaster",
-    version="0.1",
+    version="0.2",
     description="Get fiscal quarter",
     long_description="trivial class to get quarter from a user-defined gregorian fiscal year.",
     author="pathetiq kphretiq",
     author_email="kphretiq@gmail.com",
     maintainer="pathetiq kphretiq",
-    url="https://github.com/kphretiq/json-autoarray",
-    download_url="https://github.com/kphretiq/json-autoarray",
-    packages=["json_autoarray"],
+    url="https://github.com/kphretiq/quartermaster",
+    download_url="https://github.com/kphretiq/quartermaster",
+    packages=["quartermaster"],
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
